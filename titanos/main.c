957747dde2433911667fb8ada765930baf45c490
lerno <christoffer@aegik.com>
2018-07-21 22:00:22 +0200

    Initial Commit

---
+++ titanos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  titanos
+//
+//  Created by Christoffer Lerno on 2018-07-21.
+//  Copyright © 2018 Christoffer Lerno. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
